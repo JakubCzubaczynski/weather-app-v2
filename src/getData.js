a6a6df7675a67557b22009f6c5a74ceefa682236
JakubCzubaczynski <kuba33211@gmail.com>
2021-02-21 23:44:42 +0100

    u

--- src/getData.js
+++ src/getData.js
@@ -1,10 +1,6 @@
 const getData = async (url) => {
   const data = await fetch(url, {
     mode: 'cors',
-    headers: {
-      'Content-Type': 'application/json',
-      // 'Content-Type': 'application/x-www-form-urlencoded',
-    },
   }).catch((err) => console.log(err));
 
   if (data) {
